db67d4d271fb79c33aa335292b0b42d2f0169c32
caichong <87397024@qq.com>
2016-02-19 09:16:53 +0800

    Initial Commit

---
+++ ZhiMaDi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ZhiMaDi
+//
+//  Created by caichong on 16/2/19.
+//  Copyright © 2016年 ZhiMaDi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

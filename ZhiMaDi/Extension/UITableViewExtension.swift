40bf13d971ff8e0bb2a180fd5636c317309e074c
haiBear <876617048@qq.com>
2016-04-07 17:39:55 +0800

    银行卡

---
+++ ZhiMaDi/Extension/UITableViewExtension.swift
@@ -0,0 +1,13 @@
+//
+//  UITableViewExtension.swift
+//  ZhiMaDi
+//
+//  Created by haijie on 16/4/7.
+//  Copyright © 2016年 ZhiMaDi. All rights reserved.
+//
+
+import UIKit
+
+class UITableViewExtension: NSObject {
+
+}
---
+++ ZhiMaDi/UserCenter/BankCard/BankCardSetPsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  BankCardSetPsViewController.swift
+//  ZhiMaDi
+//
+//  Created by haijie on 16/4/7.
+//  Copyright © 2016年 ZhiMaDi. All rights reserved.
+//
+
+import UIKit
+
+class BankCardSetPsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
